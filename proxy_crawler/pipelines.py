cf0d63535884626277240d5eb5a310677400db42
windj007 <windj007@gmail.com>
2012-09-20 12:30:43 +0400

    log errors

--- proxy_crawler/pipelines.py
+++ proxy_crawler/pipelines.py
@@ -1,11 +1,15 @@
+import os
 from os import path
 from datetime import datetime
 import requests
+from scrapy import log
 
 class ProxyCrawlerPipeline(object):
     def __init__(self):
         res_dir = path.join(path.dirname(__file__), 'results')
-        self._res_filename = path.join(res_dir,  datetime.today().strftime('proxies_%Y_%m_%d.lst'))
+        self._res_filename = path.join(res_dir,  datetime.today().strftime('proxies_%Y_%m_%d_%H_%M.lst'))
+        if path.exists(self._res_filename):
+            os.remove(self._res_filename)
 
     def process_item(self, item, spider):
         if self.test_proxy(item['address']):
@@ -14,7 +18,8 @@ class ProxyCrawlerPipeline(object):
 
     def test_proxy(self, proxy):
         try:
-            resp = requests.head('http://google.com', proxies = { 'http', proxy })
+            resp = requests.head('http://google.com', proxies = { 'http': proxy })
             return resp.status_code < 400;
-        except:
+        except Exception as ex:
+            log.msg(ex, log.WARNING)
             return False
