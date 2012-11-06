1e1c64751fdf8773c866916cba511abd47fb3d46
windj007 <windj007@gmail.com>
2012-11-06 11:10:16 +0400

    1) retrieve proxy list with clever google request

--- proxy_crawler/pipelines.py
+++ proxy_crawler/pipelines.py
@@ -3,6 +3,8 @@ from os import path
 from datetime import datetime
 import requests
 from scrapy import log
+import threading
+
 
 class ProxyCrawlerPipeline(object):
     def __init__(self):
@@ -10,16 +12,22 @@ class ProxyCrawlerPipeline(object):
         self._res_filename = path.join(res_dir,  datetime.today().strftime('proxies_%Y_%m_%d_%H_%M.lst'))
         if path.exists(self._res_filename):
             os.remove(self._res_filename)
+        self._out_file = open(self._res_filename, 'a')
+        self._write_lock = threading.RLock()
 
     def process_item(self, item, spider):
         if self.test_proxy(item['address']):
-            with open(self._res_filename, 'a') as out_file:
-                print >> out_file, item['address']
+            with self._write_lock:
+                print >> self._out_file, item['address']
 
     def test_proxy(self, proxy):
         try:
-            resp = requests.head('http://google.com', proxies = { 'http': proxy })
+            resp = requests.head('http://ya.ru', proxies = { 'http': proxy })
             return resp.status_code < 400;
         except Exception as ex:
             log.msg(ex, log.WARNING)
             return False
+
+    def close_spider(self, spider):
+        self._out_file.flush()
+        self._out_file.close()
--- proxy_crawler/spiders/Proxy.py
+++ proxy_crawler/spiders/Proxy.py
@@ -3,18 +3,19 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from proxy_crawler.items import ProxyItem
 import re
 
+
 class ProxySpider(CrawlSpider):
     name = 'Proxy'
-    allowed_domains = ['spys.ru']
-    start_urls = ['http://www.spys.ru/en/']
-    _address_re = re.compile(r'\d+\.\d+\.\d+\.\d+\:\d+')
+    start_urls = ['http://www.google.com/search?q=%2B%C3%A2%E2%82%AC%C2%9D%3A8080%C3%A2%E2%82%AC%C2%B3+%2B%C3%A2%E2%82%AC%C2%9D%3A3128%C3%A2%E2%82%AC%C2%B3+%2B%C3%A2%E2%82%AC%C2%9D%3A80%C3%A2%E2%82%AC%C2%B3+filetype%3Atxt#sclient=psy&q=%2B%E2%80%9D:8080%E2%80%B3+%2B%E2%80%9D%3A3128%E2%80%B3+%2B%E2%80%9D%3A80%E2%80%B3+filetype%3Atxt&aq=f&aqi=&aql=&oq=%2B%E2%80%9D:8080%E2%80%B3+%2B%E2%80%9D%3A3128%E2%80%B3+%2B%E2%80%9D%3A80%E2%80%B3+filetype%3Atxt&fp=1&cad=b&sei=gICTUPX5M6jP4QSEwIGwAQ&bav=on.2,or.r_gc.r_pw.r_qf.&sei=OoOTUJyZI8iA4gS1xYGABA']
+    _address_re = re.compile(r'(\d{1,4}\.\d{1,4}\.\d{1,4}\.\d{1,4})[^0-9]+(\d+)')
     
     rules = (
-        Rule(SgmlLinkExtractor(allow = r'.*'), callback='parse_item', follow=True),
+        Rule(SgmlLinkExtractor(restrict_xpaths = '//h3[@class="r"]/a/@href'), callback='parse_list', follow=True),
     )
 
-    def parse_item(self, response):
-        for addr in ProxySpider._address_re.findall(response.body):
+    def parse_list(self, response):
+        ip_re = re.compile
+        for row in ip_re.finditer(ip_re, response.body):
             res = ProxyItem()
-            res['address'] = addr
+            res['address'] = '%s:%s' % tuple(row.groups())
             yield res
